6f9a10f9ab19a188b6973caaa0f1c4d884eb0252
Donny Gabai <mrdgabai@gmail.com>
2017-05-18 21:00:33 -0400

    last

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,61 @@
+$(document).ready(function(){
+	
+	function crystalValueGenerate(){
+		var crystalValArray = [];
+		for (i = 0; i <= 3; i++){
+			crystalValArray.push(Math.floor(Math.random() * (12-1)) + 1);
+		}
+		return crystalValArray;
+	}
+	
+	function resetValues(){
+		crystalValArray = crystalValueGenerate();
+		guessNumber = Math.floor(Math.random() * (120-19)) + 19;
+		totalScore = 0;
+		$("#totalScore").html(totalScore);
+		$("#guessNumber").html(guessNumber);
+	}
+	
+	function addTotal(crystalValue){
+		totalScore = totalScore + crystalValue;
+		$("#totalScore").html(totalScore);
+		if (totalScore > guessNumber){
+			//LOSING
+			console.log("LOST");
+			losses = losses + 1;
+			$("#losses").html("Losses: " + losses);
+			resetValues();
+		} else if (totalScore == guessNumber){
+			console.log("WIN");
+			//WINNING
+			wins = wins + 1;
+			$("#wins").html("Wins: " + String(wins));
+			resetValues();
+		}
+	}
+	
+	var crystalValArray = crystalValueGenerate();
+	var totalScore = 0;
+	var wins = 0;
+	var losses = 0;
+	var guessNumber = Math.floor(Math.random() * (120-19)) + 19;
+	
+	$("#guessNumber").html(guessNumber);
+	
+	$("#crystal1").on("click", function() {
+		addTotal(crystalValArray[0]);
+	});
+	
+	$("#crystal2").on("click", function() {
+		addTotal(crystalValArray[1]);
+	});
+	
+	$("#crystal3").on("click", function() {
+		addTotal(crystalValArray[2]);
+	});
+	
+	$("#crystal4").on("click", function() {
+		addTotal(crystalValArray[3]);
+	});
+})
 
-
-$(document).ready(function() {
-	var crystalA = 0;
-	var crystalB = 0;
-	var crystalC = 0;
-	var crystalD = 0;
-	var guessnNumber = 0;
-	var score = $("#guessnNumber");
-
-   
-        $("#guessnNumber").on("click", function() {
-        var random = Math.floor(Math.random() * 121) + 1;
-        $("#guessnNumber").html(random);
-
-});
